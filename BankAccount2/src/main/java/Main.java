71a12999a62b5cff40067ee574dafb98335fcbcd
Nhlakanipho Shusha <shushanhlakanipho@gmail.com>
2023-01-23 11:22:28 +0200

    Add files via upload

---
+++ BankAccount2/src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args){
+        BankAccount tim = new BankAccount("Tim", "A001");
+        tim.showMenu();
+
+        BankAccount gill = new BankAccount("Gill", "B002");
+        gill.showMenu();
+
+    }
+}
